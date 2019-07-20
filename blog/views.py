f52ca901e3e553ea4123080c3aba279d7af3a636
JuanHernandez0 <correo.chatarra@protonmail.com>
2019-07-19 20:49:22 -0500

    platillas {{ }}

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-  return render(request, 'blog/post_list.html', {})
+  posts = Post.objects.filter(published_date__lte=timezone.now())
+  posts = posts.order_by('published_date')
+  return render(request, 'blog/post_list.html', {'posts': posts})
 
+
+
+# test
 def test(request):
   return render(request, 'blog/test.html', {})
